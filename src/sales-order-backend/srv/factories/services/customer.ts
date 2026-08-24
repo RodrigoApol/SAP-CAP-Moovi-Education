dc89a3ad73074149bb5823cf4ddb91e232e94c6e
RodrigoApol <rodrigoap182001@gmail.com>
2026-08-24 13:44:13 -0300

    feat: applied architecture (factories)

---
+++ src/sales-order-backend/srv/factories/services/customer.ts
@@ -0,0 +1,8 @@
+import { CustomerService } from "../../services/customers/interface";
+import { CustomerServiceImpl } from "../../services/customers/implementation";
+
+const makeCustomerService = (): CustomerService => {
+    return new CustomerServiceImpl();
+}
+
+export const customerService = makeCustomerService();
