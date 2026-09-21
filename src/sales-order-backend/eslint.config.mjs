dcc47df3561c18fe1c9c4a65065a8ee03bd474da
RodrigoApol <rodrigoap182001@gmail.com>
2026-09-21 16:53:44 -0300

    feat: add ESLint configuration

---
+++ src/sales-order-backend/eslint.config.mjs
@@ -0,0 +1,53 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ['**/*.{mjs,js,ts}'],
+    // plugins: {
+    //   js,
+    // },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended
+    ],
+    languageOptions: {
+      globals: globals.node
+    },
+    ignores: ['.gen/*.{js,ts}', 'node_modules/**'],
+    // ...tseslint.configs.recommended,
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^ignore',
+          ignoreRestSiblings: true
+        }
+      ],
+      'eol-last': 'error',
+      // ident: [
+      //   'error',
+      //   4,
+      //   {
+      //     SwitchCase: 1
+      //   }
+      // ],
+      'max-len': ['error', 130],
+      'max-lines-per-function': ['warn', 30],
+      'object-curly-spacing': ['error', 'always'],
+      quotes: ['warn', 'double'],
+      'quote-props': ['error', 'as-needed'],
+      semi: ['error', 'always'],
+      'sort-imports': [
+        'warn',
+        {
+          memberSyntaxSortOrder: ['single', 'all', 'multiple', 'none'],
+          allowSeparatedGroups: true
+        }
+      ]
+    }
+  }
+]);
